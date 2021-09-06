d1efc647a07fa9398b2147e39db1a9d648987fdf
guilherme-gjv <83377948+guilherme-gjv@users.noreply.github.com>
2021-09-06 14:04:13 -0300

    feat: routes inicialized

--- app.js
+++ app.js
@@ -4,21 +4,25 @@ const handlebars = require('handlebars');
 //Body-parser foi descontinuado!!
 //const bodyParser = require('body-parser');
 const app = express();
+const admin = require('./routes/admin');
 //const mongoose = require('mongoose');
 
 //configurações
     //body-parser
-        app.use(express.urlencoded({extended: true}))
-        app.use(express.json())
-        
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
     //handlebars
-        app.engine('handlebars', handlebars({defaultLayout: 'main'}))
-    
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
     //mongoose
+
 //rotas
+app.use('/admin', admin);
 
 //outros
 const PORT = 7777;
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log("servidor rodandoo hehe");
 });

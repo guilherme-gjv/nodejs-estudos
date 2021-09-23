55c8c2e1c18c83d0d403361c52ab05911d07e0cf
guilherme-gjv <83377948+guilherme-gjv@users.noreply.github.com>
2021-09-23 16:13:40 -0300

    feat: validação dos formulários

--- app.js
+++ app.js
@@ -7,12 +7,26 @@ const app = express();
 const admin = require('./routes/admin');
 const path = require("path")
 const mongoose = require('mongoose');
+const session = require("express-session")
+const flash = require("connect-flash")
 
 //configurações
+    //session
+    app.use(session({
+        secret:"cursodenode",
+        resave: true,
+        saveUninitialized: true
+    }))
+    app.use(flash())
+    //middleware
+    app.use((req,res,next)=>{
+        res.locals.success_msg = req.flash("success_msg");
+        res.locals.error_msg = req.flash("error_msg");
+        next();
+    })
     //body-parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 
     //handlebars
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }));//
--- routes/admin.js
+++ routes/admin.js
@@ -3,33 +3,57 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const Categoria = require("../models/Categoria")
 
-router.get('/', (req,res)=>{
+
+router.get('/', (req, res) => {
     res.render('admin/index')
 });
 
-router.get('/posts', (req, res)=>{
+router.get('/posts', (req, res) => {
     res.send("Posts");
 })
 
-router.get('/categorias', (req, res)=>{
+router.get('/categorias', (req, res) => {
     res.render("admin/categorias");
 })
 
-router.post('/categorias/nova',(req,res)=>{
-    const novaCategoria = {
-        nome: req.body.nome,
-        slug: req.body.slug
+router.post('/categorias/nova', (req, res) => {
+
+    var erros = []
+
+    if (!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null) {
+        erros.push({ texto: "Nome inválido!" })
     }
 
-    //new Categoria(novaCategoria).save().then(()=>{
-    new Categoria(novaCategoria).save().then(()=>{
-        console.log('categoria criada :)');
-    }).catch((err)=>{
-        console.log('erro ao criar categoria '+err);
-    })
+    if (!req.body.slug || typeof req.body.nome == undefined || req.body.slug == null) {
+        erros.push({ texto: "slug inválido" })
+    }
+
+    if (req.body.nome.length < 2) {
+        erros.push({ texto: "Nome da categoria inválido" })
+    }
+
+    if (erros.length > 0) {
+        res.render("admin/addcategorias", { erros: erros })
+    } else {
+        const novaCategoria = {
+            nome: req.body.nome,
+            slug: req.body.slug
+        }
+
+        //new Categoria(novaCategoria).save().then(()=>{
+        new Categoria(novaCategoria).save().then(() => {
+            req.flash('success_msg', 'Categoria criada')
+            res.redirect('/admin/categorias')
+        }).catch((err) => {
+            req.flash('error_msg', 'Houve um erro ao salvar a categoria: '+err)
+            res.redirect('/admin')
+            
+        })
+    }
+
 })
 
-router.get('/categorias/add', (req, res)=>{
+router.get('/categorias/add', (req, res) => {
     res.render('admin/addcategorias');
 })
 

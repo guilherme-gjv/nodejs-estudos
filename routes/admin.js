31d4037227ba69096e4db373a87d9228f6be0813
guilherme-gjv <83377948+guilherme-gjv@users.noreply.github.com>
2021-09-23 21:28:16 -0300

    feat: listando categorias

--- routes/admin.js
+++ routes/admin.js
@@ -13,7 +13,13 @@ router.get('/posts', (req, res) => {
 })
 
 router.get('/categorias', (req, res) => {
-    res.render("admin/categorias");
+    Categoria.find().lean().then((categorias)=>{
+        res.render("admin/categorias",{categorias: categorias})
+    }).catch((err)=>{
+        res.flash("error_msg", "houve um erro ao listar as categorias ")
+        res.redirect("/admin");
+    })
+    
 })
 
 router.post('/categorias/nova', (req, res) => {

d2059580741382c87d58a414b897a325c5780428
guilherme-gjv <83377948+guilherme-gjv@users.noreply.github.com>
2021-10-21 18:10:25 -0300

    chore: autenticação do nodejs finalizada

--- config/auth.js
+++ config/auth.js
@@ -7,14 +7,14 @@ require("../models/Usuario")
 const Usuario = mongoose.model("usuarios")
 
 module.exports = function(passport){
-    passport.use(new localStrategy({usernameField:'email'}, (email, senha, donme)=>{
+    passport.use(new localStrategy({usernameField:'email', passwordField: "senha"}, (email, senha, done)=>{
         Usuario.findOne({email:email}).then((usuario)=>{
             if(!usuario){
-                return donme(null,false,{message:"esta conta não existe"})
+                return done(null,false,{message:"esta conta não existe"})
             }
             bcrypt.compare(senha, usuario.senha,(erro, batem)=>{
                 if(batem){
-                    return done(null,user)
+                    return done(null,usuario)
                 }else{
                     return done(null,false,{message:"senha incorreta"})
                 }
@@ -28,7 +28,7 @@ module.exports = function(passport){
 
     passport.deserializeUser((id,done)=>{
         Usuario.findById(id,(err,usuario)=>{
-            done(err,user)
+            done(err,usuario)
         })
     })
 }
